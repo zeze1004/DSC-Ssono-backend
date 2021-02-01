5e2dd53303250be21bd2bb7f126ecdbf013c78e2
윤소지 <yoonsoji@gmail.com>
2021-02-01 17:43:18 +0900

    create project

---
+++ src/main/java/com/ssono/diarybackend/DiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssono.diarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryBackendApplication.class, args);
+    }
+
+}
